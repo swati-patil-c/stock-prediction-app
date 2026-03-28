fc1766e2c8f1010969ecef5bdb540e7b35b6e4d3
Swati Patil <swati2006patil@gmail.com>
2026-03-28 16:52:01 +0530

    updated app.py with new code

--- Stock_Prediction/app.py
+++ Stock_Prediction/app.py
@@ -1,1 +1,9 @@
-print('hellp Prediction app')
+import yfinance as yf
+
+# take input from user
+stock_name = input("Enter stock name (e.g. AAPL, TSLA): ")
+
+# fetch data
+data = yf.download(stock_name, start="2023-01-01", end="2024-01-01")
+
+print(data.head())
